6a4621e4fc1d1506aee667f0a1159b126d17282b
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2023-07-27 23:42:53 -0300

    tarea m6u1

---
+++ tarea/m4u1y2_m5u4_m6u1/sitio_transportes/back/routes/admin/novedades.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var novedadesmodels = require ('../../models/novedadesmodels');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesmodels.getNovedades();
+  res.render('admin/novedades', { layout: 'admin/layout', persona: req.session.nombre, novedades});
+});
+router.get('/new', (req, res, next)=>{
+  res.render('admin/new',{
+    layout: 'admin/layout'});
+});
+
+router.post('/new', async (req, res, next) =>{
+  try {
+    if(req.body.titulo !='' && req.body.subtitulo !='' && req.body.cuerpo !=''){
+      await novedadesmodels.insertNovedad(req.body);
+    } else {
+      res.render ('admin/new',{layout: 'admin/layout' , error: true, message: 'Todos los campos son requeridos' })}}
+     catch (error) {
+      console.log(error)
+    res.render('admin/new', {layout: 'admin/layout', error: true, message :'No se cargo la novedad' })
+    } })
+
+
+module.exports = router;
